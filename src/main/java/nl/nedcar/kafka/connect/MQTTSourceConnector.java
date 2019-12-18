b02a361f8141194d601d335ffb067745c59781f9
Johan <johan.vandevenne@gmal.com>
2019-12-18 11:23:45 +0100

    test

--- src/main/java/nl/nedcar/kafka/connect/MQTTSourceConnector.java
+++ src/main/java/nl/nedcar/kafka/connect/MQTTSourceConnector.java
@@ -1,8 +1,35 @@
 package nl.nedcar.kafka.connect;
 
-public class MQTTSourceConnector {
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.source.SourceConnector;
 
-    public static void main(String[] args) {
+import java.util.List;
+import java.util.Map;
 
+public class MQTTSourceConnector extends SourceConnector {
+
+    public void start(Map<String, String> map) {
+
+    }
+
+    public Class<? extends Task> taskClass() {
+        return null;
+    }
+
+    public List<Map<String, String>> taskConfigs(int i) {
+        return null;
+    }
+
+    public void stop() {
+
+    }
+
+    public ConfigDef config() {
+        return null;
+    }
+
+    public String version() {
+        return null;
     }
 }
