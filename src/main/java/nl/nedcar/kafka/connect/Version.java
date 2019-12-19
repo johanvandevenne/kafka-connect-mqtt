89df4820f736ac5e4efba7988f7764a4748cfc5d
Johan <johan.vandevenne@gmal.com>
2019-12-19 11:09:40 +0100

    Packaging done

--- src/main/java/nl/nedcar/kafka/connect/Version.java
+++ src/main/java/nl/nedcar/kafka/connect/Version.java
@@ -21,20 +21,24 @@ import org.slf4j.LoggerFactory;
 import java.util.Properties;
 
 public class Version {
-  private static final Logger log = LoggerFactory.getLogger(Version.class);
-  private static String version = "unknown";
+    private static final Logger log = LoggerFactory.getLogger(Version.class);
+    private static String version = "unknown";
 
-  static {
-    try {
-      Properties props = new Properties();
-      props.load(Version.class.getResourceAsStream("/kafka-connect-mqtt-version.properties"));
-      version = props.getProperty("version", version).trim();
-    } catch (Exception e) {
-      log.warn("Error while loading version:", e);
+    static {
+        try {
+            Properties props = new Properties();
+            props.load(Version.class.getResourceAsStream("/kafka-connect-mqtt-version.properties"));
+            version = props.getProperty("version", version).trim();
+        } catch (Exception e) {
+            log.warn("Error while loading version:", e);
+        }
     }
-  }
 
-  public static String getVersion() {
-    return version;
-  }
+    public static String getVersion() {
+        return version;
+    }
+
+    public static void main(String[] args) {
+      System.out.println(Version.getVersion());
+    }
 }
