b34d6d8ed810853d6554a8c5633d76c978aa96e1
Johan <johan.vandevenne@gmal.com>
2019-12-19 10:34:02 +0100

    MQTTSourceTask done

--- src/main/java/nl/nedcar/kafka/connect/MQTTSourceTask.java
+++ src/main/java/nl/nedcar/kafka/connect/MQTTSourceTask.java
@@ -11,10 +11,12 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Actual implementation of the Kafka Connect MQTT Source Task
+ */
 public class MQTTSourceTask extends SourceTask implements IMqttMessageListener {
 
     private Logger log = LoggerFactory.getLogger(MQTTSourceConnector.class);
@@ -25,7 +27,6 @@ public class MQTTSourceTask extends SourceTask implements IMqttMessageListener {
     private IMqttClient mqttClient;
 
     public void start(Map<String, String> props) {
-        log.info("*** Enter start");
         config = new MQTTSourceConnectorConfig(props);
         mqttSourceConverter = new MQTTSourceConverter(config);
         this.sourceRecordDeque = SourceRecordDequeBuilder.of().batchSize(4096).emptyWaitMs(100).maximumCapacityTimeoutMs(60000).maximumCapacity(50000).build();
@@ -34,31 +35,36 @@ public class MQTTSourceTask extends SourceTask implements IMqttMessageListener {
 
             log.info("Connecting to MQTT Broker " + config.getString(MQTTSourceConnectorConfig.BROKER));
             mqttClient.connect();
-            log.info("Connected");
+            log.info("Connected to MQTT Broker");
 
-            String[] topicSubscriptions = (String[])this.config.getList(MQTTSourceConnectorConfig.MQTT_TOPIC).toArray(new String[0]);
-            int[] qosLevels = new int[topicSubscriptions.length];
-            IMqttMessageListener[] listeners = new IMqttMessageListener[topicSubscriptions.length];
-            // TODO Arrays.fill(qosLevels, this.config.mqttQos);
-            Arrays.fill(listeners, this);
+            String topicSubscription = this.config.getString(MQTTSourceConnectorConfig.MQTT_TOPIC);
+            int qosLevel = this.config.getInt(MQTTSourceConnectorConfig.MQTT_QOS);
 
-            log.info("Subscribing to " + topicSubscriptions[0]);
-            mqttClient.subscribe(topicSubscriptions, listeners);
+            log.info("Subscribing to " + topicSubscription + " with QOS " + qosLevel);
+            mqttClient.subscribe(topicSubscription, qosLevel, this);
+            log.info("Subscribed to " + topicSubscription + " with QOS " + qosLevel);
         }
         catch (MqttException e) {
             throw new ConnectException(e);
         }
     }
 
+    /**
+     * method is called periodically by the Connect framework
+     *
+     * @return
+     * @throws InterruptedException
+     */
     public List<SourceRecord> poll() throws InterruptedException {
         List<SourceRecord> records = sourceRecordDeque.getBatch();
-        log.info("Records: " + records);
+        log.trace("Records returning to poll(): " + records);
         return records;
     }
 
     public void stop() {
         if (mqttClient.isConnected()) {
             try {
+                log.debug("Disconnecting from MQTT Broker " + config.getString(MQTTSourceConnectorConfig.BROKER));
                 mqttClient.disconnect();
             } catch (MqttException mqttException) {
                 log.error("Exception thrown while disconnecting client.", mqttException);
@@ -70,11 +76,15 @@ public class MQTTSourceTask extends SourceTask implements IMqttMessageListener {
         return Version.getVersion();
     }
 
+
+    /**
+     * Callback method when a MQTT message arrives at the Topic
+     */
     @Override
     public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-        log.info("Message arrived in connector from topic " + topic);
+        log.trace("Message arrived in connector from topic " + topic);
         SourceRecord record = mqttSourceConverter.convert(topic, mqttMessage);
-        log.info("Converted record: " + record);
+        log.trace("Converted record: " + record);
         sourceRecordDeque.add(record);
     }
 }
